16cb600a668cb73b6dc75d6904df8cc9543969b8
Tolfx <matteus@tolfx.dev>
2022-01-16 14:28:22 +0100

    Added new stuff to CPG_NPM

--- src/Index.ts
+++ src/Index.ts
@@ -1,7 +1,17 @@
 import CacheModules from "./Modules/Cache/Cache.modules";
+import CategoriesDelete from "./REST/Categories/Categories.delete";
+import CategoriesGet from "./REST/Categories/Categories.get";
+import CategoriesPost from "./REST/Categories/Categories.post";
+import CategoriesPut from "./REST/Categories/Categories.put";
 
 const CPG_NPM = {
-    config: {...CacheModules}
+    config: {...CacheModules},
+    categories: {
+        get: CategoriesGet,
+        post: CategoriesPost,
+        delete: CategoriesDelete,
+        put: CategoriesPut
+    }
 }
 
 export = CPG_NPM;
