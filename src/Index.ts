1445bca9789b1d225c8adbc9fb5d571b7b347cb1
Tolfx <matteus@tolfx.dev>
2022-01-16 14:41:09 +0100

    Added customer

--- src/Index.ts
+++ src/Index.ts
@@ -3,6 +3,10 @@ import CategoriesDelete from "./REST/Categories/Categories.delete";
 import CategoriesGet from "./REST/Categories/Categories.get";
 import CategoriesPost from "./REST/Categories/Categories.post";
 import CategoriesPut from "./REST/Categories/Categories.put";
+import CustomerDelete from "./REST/Customer/Customer.delete";
+import CustomerGet from "./REST/Customer/Customer.get";
+import CustomerPost from "./REST/Customer/Customer.post";
+import CustomerPut from "./REST/Customer/Customer.put";
 
 const CPG_NPM = {
     config: {...CacheModules},
@@ -11,6 +15,12 @@ const CPG_NPM = {
         post: CategoriesPost,
         delete: CategoriesDelete,
         put: CategoriesPut
+    },
+    customers: {
+        get: CustomerGet,
+        post: CustomerPost,
+        delete: CustomerDelete,
+        put: CustomerPut,
     }
 }
 
